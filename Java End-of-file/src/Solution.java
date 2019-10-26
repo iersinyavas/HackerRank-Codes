c5282d53d9bc8d189d50d0e482bdcada7a9a141c
İbrahim Ersin YAVAŞ <iersinyavas@gmail.com>
2019-10-26 16:12:10 +0300

    Java End-of-file codes

---
+++ Java End-of-file/src/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int i = 1;
+
+		while (scanner.hasNext()) {
+			System.out.println(i + " " + scanner.nextLine());
+			i++;
+		}
+		scanner.close();
+	}
+}
